fd3852679dee03d7369c854ee312fdaedd5e8c97
Sabbir Hasan <xofficial490@gmail.com>
2025-09-15 13:11:03 +0600

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ViteSitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-	plugins: [tailwindcss(), react()],
-})
+	plugins: [
+		tailwindcss(),
+		react(),
+		ViteSitemap({
+			hostname: 'https://www.sheetlypro.com',
+		}),
+	],
+})	
