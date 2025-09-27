156ba96f0108ee051b22f0b0bce0736cdf237ec1
Sabbir Hasan <xofficial490@gmail.com>
2025-09-28 01:12:28 +0600

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,12 @@ import AppRoutes from './routes/AppRoutes.jsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<AuthProvider>
-				<BrowserRouter>
-					<AppRoutes />
-				</BrowserRouter>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
 		</AuthProvider>
 	</StrictMode>,
 )
