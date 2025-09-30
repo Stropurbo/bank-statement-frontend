7bebf59daa5d18fe53b64e681a28c84cf0b87803
Sabbir Hasan <xofficial490@gmail.com>
2025-09-30 16:30:33 +0600

    bug fix

--- src/pages/AdminStatements.jsx
+++ src/pages/AdminStatements.jsx
@@ -37,7 +37,8 @@ function AdminStatements() {
 			const url = window.URL.createObjectURL(new Blob([response.data]))
 			const link = document.createElement('a')
 			link.href = url
-			link.setAttribute('download', filename)
+			const csvFilename = filename ? filename.replace(/\.[^/.]+$/, '.csv') : `statement_${statementId}.csv`
+			link.setAttribute('download', csvFilename)
 			document.body.appendChild(link)
 			link.click()
 			link.remove()
